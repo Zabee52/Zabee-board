05db8e96f110cdd0535a222f5bf678dce1072a5c
Zabee52 <zabeee52@gmail.com>
2022-02-16 22:24:20 +0900

    22. 2. 16. 프로그래머스 문제풀이

---
+++ algorithm/java/src/Programmers/Tes.java
@@ -0,0 +1,34 @@
+package Programmers;
+
+import java.util.Locale;
+
+public class Tes {
+    public static String solution(String s) {
+        s = s.toLowerCase(Locale.ROOT);
+        int[] arr = new int[26];
+        for (char c : s.toCharArray()) {
+            arr[c - 97]++;
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i : arr) {
+            if (max < i) {
+                max = i;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max) {
+                result.append((char) (i + 97));
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aAb"));
+    }
+}
